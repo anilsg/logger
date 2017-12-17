a5aad131530fa03fafe30be45132dad06da69258
Neil <anilsg@users.noreply.github.com>
2017-12-17 20:56:59 +1100

    Convert request response to class

--- logger_resource.py
+++ logger_resource.py
@@ -4,67 +4,61 @@ import os
 log_path = '/srv/logger'
 log_directory = os.path.join(log_path, 'logs') # Available logs.
 
-def split_min(url, sep='/', minvals=3):
-    for tok in url.split(sep):
+def split_min(req, sep='/', minvals=3):
+    for tok in req.split(sep):
         minvals -= 1
         yield tok
     while minvals > 0:
         minvals -= 1
         yield ''
 
-def get_filter(url):
-    (since, until, levels, *facilities) = split_min(url[15:])
-    print("since/until/levels/facilities:", since, '/', until, '/', levels, '/', facilities)
-
-    (since, start_time) = split_min(since, sep='-', minvals=2)
-    print("since / start_time:", since, '/', start_time)
-
-    (until, stop_time) = split_min(until, sep='-', minvals=2)
-    print("until / stop_time:", until, '/', stop_time)
-
-    (start_level, stop_level) = split_min(levels, sep='-', minvals=2)
-    print("start_level / stop_level:", start_level, '/', stop_level)
-
-get_filter('/api/v1/counts/20171205-134200/20171205-134223/30-40/facility_one')
-
-def get_counts():
+class get_filter():
     """
-    E.g. curl -i http://localhost:8080/api/v1/counts/20171205/02/?name=facility_one
-    /api/v1/counts/YYYYMMDD[HH[MM]]/DD[HH[MM]]/[levelno[-levelno]]/[facility_name]
-    1. breakdown URL into parts
-    2. cycle through available files
-    3. if match level, facility, date
-    4. open read count matching lines
+    Instead of since duration use since until
+    /api/v1/counts/since/until/levels/names
+    url = '/api/v1/counts/20171205-134200/20171205-134223/30-40/facility_one'
     """
-    url = '/api/v1/counts/YYYYMMDD-HHMMSS/YYYYMMDD-HHMMSS/level-level/facility_name...'
-    since = '20171205-134200'
-    until = '20171205-134223'
-    levels = '30-40'
-    facility = 'facility_one'
+    def __init__(self, url):
+        url = url[15:]
+        (since, until, levels, *facilities) = split_min(url, sep='/', minvals=3)
+        (self.since, self.start_time) = split_min(since, sep='-', minvals=2)
+        (self.until, self.stop_time) = split_min(until, sep='-', minvals=2)
+        (self.start_level, self.stop_level) = split_min(levels, sep='-', minvals=2)
+        self.facilities = facilities
 
-    since, start_time = since.split('-')
-    until, stop_time = until.split('-')
-    start_level, stop_level = levels.split('-')
-    facilities = (facility,)
+    def get_counts(self):
+        """
+        E.g. curl -i http://localhost:8080/api/v1/counts/20171205/02/?name=facility_one
+        /api/v1/counts/YYYYMMDD[HH[MM]]/DD[HH[MM]]/[levelno[-levelno]]/[facility_name]
+        1. breakdown URL into parts
+        2. cycle through available files
+        3. if match level, facility, date
+        4. open read count matching lines
+        Allow empty non supplied values to indicate 'all'.
+        self.since, self.start_time, self.until, self.stop_time
+        self.start_level, self.stop_level, self.facilities
+        """
+        message_count = 0
+        log_list = os.listdir(log_directory)
+        for log_name in log_list:
+            (day, level, facility) = log_name.split('-')
+            if self.since and day < self.since: continue
+            if self.until and day > self.until: break
+            if self.start_level and level < self.start_level: continue
+            if self.stop_level and level > self.stop_level: continue
+            if self.facilities and facility not in self.facilities: continue
+            print("reading", log_name) # DEBUG
+            start_time = day == self.since and self.start_time or ''
+            stop_time = day == self.until and self.stop_time or ''
+            with open(os.path.join(log_directory, log_name), mode='r') as log_file:
+                for log_line in log_file:
+                    stamp = log_line.split('.')[0].split('-')[1]
+                    if start_time and stamp < start_time: continue
+                    if stop_time and stamp > stop_time: continue
+                    message_count += 1
+        self.message_count = message_count
 
-    message_count = 0
-    log_list = os.listdir(log_directory)
-    for log_name in log_list:
-        (day, level, facility) = log_name.split('-')
-        if day < since: continue
-        if day > until: break
-        if level < start_level: continue
-        if level > stop_level: continue
-        if facility not in facilities: continue
-        print("reading", log_name) # DEBUG
-        start_time = day == since and start_time or ''
-        stop_time = day == until and stop_time or ''
-        with open(os.path.join(log_directory, log_name), mode='r') as log_file:
-            for log_line in log_file:
-                stamp = log_line.split('.')[0].split('-')[1]
-                if start_time and stamp < start_time: continue
-                if stop_time and stamp > stop_time: continue
-                message_count += 1
-    return message_count
-
-#print(get_counts())
+url = '/api/v1/counts/20171205-134200/20171205-134223/30-40/facility_one'
+filtered = get_filter(url)
+filtered.get_counts()
+print(filtered.message_count)
