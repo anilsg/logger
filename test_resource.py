7e516c00b700b87034eadd6adafc4a584228c19e
Neil <anilsg@users.noreply.github.com>
2018-01-24 14:41:28 +1100

    Update tests to match changed interface.

--- test_resource.py
+++ test_resource.py
@@ -8,8 +8,11 @@ Anil Gulati
 
 import logger_resource
 
-def test_split_min_empty():
-    assert list(logger_resource.split_min('')) == ['', '', '']
+def test_split_min_empty_default():
+    assert list(logger_resource.split_min('')) == ['', '', '', '']
+
+def test_split_min_empty_3():
+    assert list(logger_resource.split_min('', minvals=3)) == ['', '', '']
 
 def test_split_min_standard():
     assert list(logger_resource.split_min('/api/v1/counts/20171205-134200/20171205-134223/30-40/facility_one'[8:])) == ['counts', '20171205-134200', '20171205-134223', '30-40', 'facility_one']
@@ -21,6 +24,6 @@ def test_split_min_missing_values():
     assert list(logger_resource.split_min('20171205', sep='-', minvals=2)) == ['20171205', '']
 
 def test_get_filter_init():
-    filtered = logger_resource.get_filter('/api/v1/counts/20171205-134200/20171205-134223/30-40/facility_one/facility_two/facility_three/')
+    filtered = logger_resource.GetFilter('/api/v1/counts/20171205-134200/20171205-134223/30-40/facility_one/facility_two/facility_three/')
     assert filtered.facilities == ['facility_one', 'facility_two', 'facility_three', '']
 
